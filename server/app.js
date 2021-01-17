c823ddf80f9f574d8bbcbdb6ff69402caec4e1e0
랄라수 <lallasu@lallasuui-MacBookPro.local>
2021-01-17 21:24:32 +0900

    express start

---
+++ server/app.js
@@ -0,0 +1,71 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import config from "./config";
+// import hpp from "hpp";
+// import helmet from "helmet";
+// import cors from "cors";
+// import path from "path";
+
+// // Routes
+// import postRoutes from "./routes/api/post";
+// import userRoutes from "./routes/api/user";
+// import authRoutes from "./routes/api/auth";
+// import searchRoutes from "./routes/api/search";
+
+// import morgan from "morgan";
+
+// const app = express();
+// const { MONGO_URI } = config;
+
+// const prod = process.env.NODE_ENV === "production";
+
+// app.use(hpp());
+// app.use(helmet());
+
+// app.use(cors({ origin: true, credentials: true }));
+// app.use(morgan("dev"));
+
+// app.use(express.json());
+
+// mongoose
+//   .connect(MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => console.log("MongoDB connecting Success!!"))
+//   .catch((e) => console.log(e));
+
+// // Use routes
+// app.all("*", (req, res, next) => {
+//   let protocol = req.headers["x-forward-proto"] || req.protocol;
+//   if (protocol === "https") {
+//     next();
+//   } else {
+//     let to = `https://${req.hostname}${req.url}`;
+//     res.redirect(to);
+//   }
+// });
+
+// app.use("/api/post", postRoutes);
+// app.use("/api/user", userRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/search", searchRoutes);
+
+// if (prod) {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+//   });
+// }
+
+// export default app;
+
+import express from 'express'
+
+const app = express()
+
+app.get('/')
+
+export default app;
