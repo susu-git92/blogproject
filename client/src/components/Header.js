d99f093e11634a4911412af383acae5725fc82b9
랄라수 <lallasu@lallasuui-MacBookPro.local>
2021-02-09 23:09:59 +0900

    header photo added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+// import {Row, Col}from 'reactstrap'
+import '../assets/custom.css'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <PageHeader id="page-header" className="mb-3">
+            <div>
+                <div md="6" sm="auto" className="text-center m-auto">
+                    <h1>Read Our Blog</h1>
+                    <p>랄라수의 작업 블로그입니다</p>
+                </div>
+            </div>
+        </PageHeader>
     )
 }
 export default Header;
+
+const PageHeader=styled.div``
